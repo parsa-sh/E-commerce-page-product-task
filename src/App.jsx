2ce8c732b82c74708f17f4febb0fde4ec028a265
Parsa_Shahsavan <shahsavanparsa4@gmail.com>
2024-10-07 05:06:58 +0330

    Creating App component's UI and add Review component to it

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
+import { Stack, VStack } from "@chakra-ui/react";
 import ProductDetail from "./components/ProductDetail";
+import Review from "./components/Review";
+import './app.css'
 
-export default function App() {
+function App() {
   return (
-    <div>
-      <ProductDetail/>
+    <div className="container">
+      <VStack
+        color={{ md: "white" }}
+        width={{ xl: "800px", lg: "700px", md: "600px", base: "100%" }}
+        borderRadius={{ md: "24px", base: "18px" }}
+        bgColor={{ md: "#3d3d3dd1", base: "white" }}
+        padding={{ lg: "38px", base: "14px" }}
+        boxShadow="0 0 30px #262626d9"
+        gap="50px"
+        
+      >
+        <ProductDetail />
+        <Stack
+          direction="row"
+          width="100%"
+          justifyContent="space-around"
+          borderTop="solid 3px black"
+        >
+          <Review />
+        </Stack>
+      </VStack>
     </div>
-  )
+  );
 }
+
+export default App;
